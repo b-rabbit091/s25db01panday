1b17b02b988406c3945c9e513bc23e24a3c0bf05
Anil Panday <34656068+b-rabbit091@users.noreply.github.com>
2025-04-16 19:06:39 -0500

    till ss9

--- controllers/costume.js
+++ controllers/costume.js
@@ -139,3 +139,16 @@ res.send(`{'error': '${err}'}`);
 }
 };
 
+// Handle a delete one view with id from query
+exports.costume_delete_Page = async function(req, res) {
+console.log("Delete view for id " + req.query.id)
+try{
+result = await Costume.findById(req.query.id)
+res.render('costumedelete', { title: 'Costume Delete', toShow:
+result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
